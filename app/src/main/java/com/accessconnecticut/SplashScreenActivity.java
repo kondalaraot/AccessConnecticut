d9c6f67c59f5396de04915af4849bc2078e32717
KR <kondal.rao58@gmail.com>
2017-01-05 16:47:01 +0530

    Add Animation test zoomin

--- app/src/main/java/com/accessconnecticut/SplashScreenActivity.java
+++ app/src/main/java/com/accessconnecticut/SplashScreenActivity.java
@@ -1,11 +1,12 @@
 package com.accessconnecticut;
 
 import android.app.KeyguardManager;
-import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import java.security.KeyStore;
 
@@ -28,15 +29,66 @@ public class SplashScreenActivity extends AppCompatActivity {
     private Cipher cipher;
     private FingerprintManager.CryptoObject cryptoObject;
 
+    Animation zoomin, zoomout; //declared as public
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        zoomin = AnimationUtils.loadAnimation(this, R.anim.zoomin);
+//        zoomout = AnimationUtils.loadAnimation(this, R.anim.zoomout);
 
-        new Handler().postDelayed(new Runnable() {
+        imageView.setAnimation(zoomin);
+
+//        imageView.setAnimation(zoomin);
+
+        /*zoomin.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                imageView.startAnimation(zoomout);
+
+            }
+        });*/
+
+        /*zoomout.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+//                imageView.startAnimation(zoomin);
+
+            }
+        });*/
+
+     /*   new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
+                *//* Create an Intent that will start the Menu-Activity. *//*
 
                 Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(mainIntent);
@@ -44,7 +96,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
 
             }
-        }, SPLASH_DISPLAY_LENGTH);
+        }, SPLASH_DISPLAY_LENGTH);*/
     }
 
 
